629916034ba98b3539747cbc029cc86a6d1b776d
FROSSTY <aom_jackal@hotmail.com>
2023-01-04 10:33:46 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,61 @@
+from flask import Flask ,json,request,jsonify
+import os
+import urllib.request
+from werkzeug.utils import secure_filename
+
+app = Flask(__name__)
+
+app.secret_key = "caircocoders-ednalan"
+
+UPLOAD_FLODER = 'static/upload'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FLODER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+
+ALLOWED_EXTENSIONS = set(['txt','pdf','png','jpg','jpeg','gif'])
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/')
+def main():
+    return 'Homepage'
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    #check post file path
+    if 'files[]' not in request.files:
+        resp = jsonify({'message' : 'No File part in the request'})
+        resp.status_code = 400
+        return resp
+
+    files = request.files.getlist('files[]')
+
+    errors = {}
+    success = False
+
+    for file in files:
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            success = True
+        else:
+            errors[file.filename] = 'File Type is not allowed'
+
+    if success and errors:
+        errors['message'] = 'File(s) successfully uploaded'
+        resp = jsonify(errors)
+        resp.status_code = 500
+        return resp
+    if success:
+        resp=jsonify({'message' : 'File successfully uploaded'})
+        resp.status_code = 201
+        return resp
+    else:
+        resp = jsonify(errors)
+        resp.status_code = 500
+        return resp
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
